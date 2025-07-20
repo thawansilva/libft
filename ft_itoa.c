1dd3a2187c7ff5ffeb360dde965011a860bfdc0e
Thawan Silva <thaperei@student.42sp.org.br>
2025-07-20 09:22:44 -0300

    New implemented functions

---
+++ ft_itoa.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/19 12:25:21 by thaperei          #+#    #+#             */
+/*   Updated: 2025/07/19 12:25:21 by thaperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./libft.h"
+
+static int	ft_getsize(int n)
+{
+	int	size;
+
+	size = 0;
+	if (n <= 0)
+	{
+		size++;
+		n = -n;
+	}
+	while (n != 0)
+	{
+		size++;
+		n /= 10;
+	}
+	return (size);
+}
+
+char	*ft_itoa(int n)
+{
+	int		sign;
+	int		size;
+	long	num;
+	char	*nbr;
+
+	num = n;
+	sign = 1;
+	size = ft_getsize(num);
+	nbr = (char *) malloc(size + 1);
+	nbr[size] = '\0';
+	if (num < 0)
+	{
+		sign = -1;
+		num = -num;
+	}
+	while (size--)
+	{
+		nbr[size] = num % 10 + '0';
+		num /= 10;
+	}
+	if (sign < 0)
+		nbr[0] = '-';
+	return (nbr);
+}
